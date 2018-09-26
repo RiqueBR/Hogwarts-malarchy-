f459904822218d1a78acc5e10ae9881ab0642150
Henrique Pereira Batista <h.p.batista20@gmail.com>
2018-09-26 22:12:52 +0100

    finished .all for house class

--- hogwarts_controller.rb
+++ hogwarts_controller.rb
@@ -10,13 +10,19 @@ get '/students' do
   erb(:index)
 end
 
+## Q1. Why do I need to gather all my objects of houses before goinf to my
+## .erb file?
+
 get '/students/new' do
   @houses = House.find_all()
   erb(:new)
 end
 
+## Q2. Does post methods don't need a erb to go to because I'm
+## sending the information?
+
+
 post '/students' do
-  @houses = House.find_all()
   @student = Student.new(params)
   @student.save
   redirect '/students'
